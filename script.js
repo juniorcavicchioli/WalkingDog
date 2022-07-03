cd30dfad9867c8c99dfcc47171e439a7e4053941
Adilson Roberto Cavicchioli Junior <101985616+juniorcavicchioli@users.noreply.github.com>
2022-07-02 22:41:54 -0300

    Melhorias no CSS e uma pequena mudança no JS
    - Adicionei estilo ao botão que faz o cachorro "andar";
    - Também coloquei valores mais precisos no script para resolver um problema em que, quando andava, ele poderia sair um pouco da tela;
    - Adicionei um fundo que se mescla com o cenário para deixar a página visualmente mais atraente;
    - Adicionei um link que visualmente se parece com um botão no rodapé da página que direciona ao meu perfil (para o funcionamento é inútil, apenas para treino)

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function myMove(){
     var elem = document.getElementById("dog-path");
     var pos = 0;
     var id = setInterval(frame, 5);
-    document.querySelector("#cachorro").src = "dog.gif";
+    document.querySelector("#dog").src = "dog.gif";
     function frame(){
-        if(pos >= 83) {
+        if(pos >= 81.5) {
             clearInterval(id);
-            document.querySelector("#cachorro").src = "dogleft.gif";
+            document.querySelector("#dog").src = "dogleft.gif";
             var id2 = setInterval(voltar, 5);
             function voltar(){
                 if(pos<=0){
-                    document.querySelector("#cachorro").src = "dog1.gif";
+                    document.querySelector("#dog").src = "dog2.gif";
                     clearInterval(id2);
                 } else {
                     pos -= 0.1;
